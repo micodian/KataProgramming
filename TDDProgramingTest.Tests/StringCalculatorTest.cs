1832d59d43f51d0cec523c396199497e1b371cea
micodian <micodian83@gmail.com>
2023-01-08 12:33:33 -0600

    added tests to handle new line delimeter

---
+++ TDDProgramingTest.Tests/StringCalculatorTest.cs
@@ -0,0 +1,78 @@
+using TDDProgramming.Kata;
+
+namespace TDDProgramingTest.Tests
+{
+    [TestClass]
+    public class StringCalculatorTest
+    {
+        private StringCalculator? _stringCalculator;
+        [TestInitialize]
+        public void SetUp()
+        {
+            _stringCalculator = new StringCalculator();
+        }
+
+        [TestMethod]
+        public void StringCalculator_Should_Be_Initialized()
+        {
+            Assert.IsNotNull(_stringCalculator);
+        }
+
+        [TestMethod]
+        public void StringCalculator_Add_Method_Should_Return_0_For_Empty_Input()
+        {
+            //Arrange
+            string input = "";
+            //Act
+            var result = _stringCalculator?.Add(input);
+            //Assert
+            Assert.AreEqual(0, result);
+        }
+
+
+        [TestMethod]
+        public void StringCalculator_Add_Method_Should_Return_For_Single_Input()
+        {
+            //Arrange
+            string input = "1";
+            //Act
+            var result = _stringCalculator?.Add(input);
+            //Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void StringCalculator_Add_Method_Should_Return_For_Two_Numbers()
+        {
+            //Arrange
+            string input = "1,2";
+            //Act
+            var result = _stringCalculator?.Add(input);
+            //Assert
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void StringCalculator_Add_Method_Should_Return_For_Multiple_Numbers()
+        {
+            //Arrange
+            string input = "1,2,3,4,5,6,7";
+            //Act
+            var result = _stringCalculator?.Add(input);
+            //Assert
+            Assert.AreEqual(28, result);
+        }
+
+        [TestMethod]
+        public void StringCalculator_Add_Method_Should_Return_Using_NewLine_Delimeter()
+        {
+            //Arrange
+            string input = "1\n2,3";
+            //Act
+            var result = _stringCalculator?.Add(input);
+            //Assert
+            Assert.AreEqual(6, result);
+            //test
+        }
+    }
+}
---
+++ TDDProgramming/Kata/StringCalculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDProgramming.Kata
+{
+    public class StringCalculator
+    {
+        public int Add(string input)
+        {
+            int sumOfNumbers = 0;
+            if (string.IsNullOrEmpty(input))
+            {
+                return sumOfNumbers;
+            }
+            var splitInputString = input.Split(',', '\n');
+            sumOfNumbers = GetSumOfNumbers(splitInputString);
+            return sumOfNumbers;
+        }
+
+        private static int GetSumOfNumbers(string[] splitInputString)
+        {
+            int sum = 0;
+            foreach (var number in splitInputString)
+            {
+                sum += int.Parse(number);
+            }
+            return sum;
+            
+        }
+    }
+}
